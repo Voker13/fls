25b7a58480b9b8c7215a68366789f924d564e181
Tobi <tobi.gartmann@web.de>
2016-03-21 19:29:24 +0100

    now reading txt

--- FLS/src/pack/Location.java
+++ FLS/src/pack/Location.java
@@ -20,121 +20,138 @@ public class Location {
     public double angle;
     public int durationToBacker;
 
-    public Location() {}
+    public Location() {
+    }
 
     public boolean isDepto() {
-        return this.id == 0;
+	return this.id == 0;
     }
 
     public String toString() {
-        return String.format("%f %f %d %d %d %s", longitude, latitude, visitDuration, timeFromDepot, timeToDepot, address);
+	return String.format("%f %f %d %d %d %s", longitude, latitude, visitDuration, timeFromDepot, timeToDepot, address);
     }
 
     private static class InstanceReader {
-        private InputStreamReader in;
-        private static final int BUFFER_SIZE = 8192;
-        private char[] buf;
-        private int pos; // index of next unread char = number of already used chars in buffer
-        private int full; // number of chars in buffer
+	private InputStreamReader in;
+	private static final int BUFFER_SIZE = 8192;
+	private char[] buf;
+	private int pos; // index of next unread char = number of already used
+			 // chars in buffer
+	private int full; // number of chars in buffer
 
-        public InstanceReader(String filename) throws FileNotFoundException, IOException {
-            try {
-                in = new InputStreamReader(new FileInputStream(new File(filename)), "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                in = new InputStreamReader(new FileInputStream(new File(filename)));
-            }
-            buf = new char[BUFFER_SIZE];
-            pos = 0;
-            full = in.read(buf, 0, BUFFER_SIZE);
-        }
+	// private String testFile = "./Instance-1000-200.txt";
 
-        protected int ensureAvailable(int num) throws IOException {
-            int avail = full - pos;
-            if(avail >= num || full != BUFFER_SIZE) {
-                return avail;
-            }
-            System.arraycopy(buf, pos, buf, 0, avail);
-            pos = 0;
-            full = avail;
-            int read = in.read(buf, avail, BUFFER_SIZE - avail);
-            if(read > 0) {
-                full += read;
-            }
-            return full;
-        }
+	public InstanceReader(String filename) throws FileNotFoundException, IOException {
+	    // InputStream inputStream = null;
+	    // OutputStream outputStream = null;
+	    try {
+		in = new InputStreamReader(new FileInputStream(new File(filename)), "UTF-8");
+	    } catch (UnsupportedEncodingException e) {
+		in = new InputStreamReader(new FileInputStream(new File(filename)));
+	    }
+	    /*
+	     * inputStream = new FileInputStream(new File(filename));
+	     * 
+	     * // write the inputStream to a FileOutputStream outputStream = new
+	     * FileOutputStream(new File(testFile));
+	     * 
+	     * int read = 0; byte[] bytes = new byte[1024]; outputStream = new
+	     * FileOutputStream(new File(testFile)); while ((read =
+	     * inputStream.read(bytes)) != -1) { outputStream.write(bytes, 0,
+	     * read); } inputStream.close(); outputStream.close();
+	     */
+	    buf = new char[BUFFER_SIZE];
+	    pos = 0;
+	    full = in.read(buf, 0, BUFFER_SIZE);
+	}
 
-        public int readInt(char nextChar) throws IOException {
-            ensureAvailable(10);
-            int num = 0;
-            char c;
-            while((c = buf[pos++]) != nextChar) {
-                num = num * 10 + (c - '0');
-            }
-            return num;
-        }
+	protected int ensureAvailable(int num) throws IOException {
+	    int avail = full - pos;
+	    if (avail >= num || full != BUFFER_SIZE) {
+		return avail;
+	    }
+	    System.arraycopy(buf, pos, buf, 0, avail);
+	    pos = 0;
+	    full = avail;
+	    int read = in.read(buf, avail, BUFFER_SIZE - avail);
+	    if (read > 0) {
+		full += read;
+	    }
+	    return full;
+	}
 
-        public double readDouble(char nextChar) throws IOException {
-            ensureAvailable(34);
-            char c;
-            int num = 0;
-            while((c = buf[pos++]) != '.') {
-                num = num * 10 + (c - '0');
-            }
-            int denom = 1;
-            while((c = buf[pos++]) != nextChar) {
-                num = num * 10 + (c - '0');
-                denom *= 10;
-            }
-            return (double)num / (double)denom;
-        }
+	public int readInt(char nextChar) throws IOException {
+	    ensureAvailable(10);
+	    int num = 0;
+	    char c;
+	    while ((c = buf[pos++]) != nextChar) {
+		num = num * 10 + (c - '0');
+	    }
+	    return num;
+	}
 
-        public String readString(char nextChar) throws IOException {
-            StringBuilder sb = new StringBuilder();
-            while(true) {
-                int start = pos;
-                while(pos < full) {
-                    if(buf[pos] == nextChar) {
-                        sb.append(buf, start, pos - start);
-                        pos++; // consume nextChar
-                        return sb.toString();
-                    }
-                    pos++;
-                }
-                sb.append(buf, start, pos - start);
-                if(full != BUFFER_SIZE) {
-                    return sb.toString();
-                }
-                pos = 0;
-                int read = in.read(buf, 0, BUFFER_SIZE);
-                if(read > 0) {
-                    full = read;
-                } else {
-                    full = 0;
-                }
-            }
-        }
+	public double readDouble(char nextChar) throws IOException {
+	    ensureAvailable(34);
+	    char c;
+	    int num = 0;
+	    while ((c = buf[pos++]) != '.') {
+		num = num * 10 + (c - '0');
+	    }
+	    int denom = 1;
+	    while ((c = buf[pos++]) != nextChar) {
+		num = num * 10 + (c - '0');
+		denom *= 10;
+	    }
+	    return (double) num / (double) denom;
+	}
 
-        public void close() throws IOException {
-            in.close();
-        }
+	public String readString(char nextChar) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    while (true) {
+		int start = pos;
+		while (pos < full) {
+		    if (buf[pos] == nextChar) {
+			sb.append(buf, start, pos - start);
+			pos++; // consume nextChar
+			return sb.toString();
+		    }
+		    pos++;
+		}
+		sb.append(buf, start, pos - start);
+		if (full != BUFFER_SIZE) {
+		    return sb.toString();
+		}
+		pos = 0;
+		int read = in.read(buf, 0, BUFFER_SIZE);
+		if (read > 0) {
+		    full = read;
+		} else {
+		    full = 0;
+		}
+	    }
+	}
+
+	public void close() throws IOException {
+	    in.close();
+	}
     }
 
     public static List<Location> readInstance(String filename) throws FileNotFoundException, IOException {
-        Location.InstanceReader ir = new Location.InstanceReader(filename);
-        int n = ir.readInt('\n');
-        List<Location> nodes = new ArrayList<Location>(n);
-        for(int i = 0;i < n;i++) {
-            Location node = new Location();
-            node.id = i;
-            node.longitude = ir.readDouble(' ');
-            node.latitude = ir.readDouble(' ');
-            node.visitDuration = ir.readInt(' ');
-            node.timeFromDepot = ir.readInt(' ');
-            node.timeToDepot = ir.readInt(' ');
-            node.address = ir.readString('\n');
-            nodes.add(node);
-        }
-        ir.close();
-        return nodes;
+	Location.InstanceReader ir = new Location.InstanceReader(filename);
+	int n = ir.readInt('\n');
+	List<Location> nodes = new ArrayList<Location>(n);
+	for (int i = 0; i < n; i++) {
+	    Location node = new Location();
+	    node.id = i;
+	    node.longitude = ir.readDouble(' ');
+	    node.latitude = ir.readDouble(' ');
+	    node.visitDuration = ir.readInt(' ');
+	    node.timeFromDepot = ir.readInt(' ');
+	    node.timeToDepot = ir.readInt(' ');
+	    node.address = ir.readString('\n');
+	    nodes.add(node);
+	}
+	ir.close();
+	return nodes;
     }
 }
--- FLS/src/pack/Main.java
+++ FLS/src/pack/Main.java
@@ -5,158 +5,159 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static ArrayList<Location> nodes;
-	public static Location depot;
-	public static int speed = 0;
-	public static int solution = 0;
+    public static ArrayList<Location> nodes;
+    public static Location depot;
+    public static int speed = 0;
+    public static int solution = 0;
 
-	public static void main(String[] args) throws IOException {
+    private static String testFile = "./Instance-1000-200.txt";
 
-		String file = null;
+    public static void main(String[] args) throws IOException {
 
-		System.out.println("INSTANCE txt");
+	// String file = null;
+	// System.out.println("INSTANCE txt");
+	// file = readFromSystemIn();
+	// nodes = (ArrayList<Location>) Location.readInstance(file);
 
-		file = readFromSystemIn();
+	nodes = (ArrayList<Location>) Location.readInstance(testFile);
 
-		nodes = (ArrayList<Location>) Location.readInstance(file);
+	setDepot();
 
-		setDepot();
+	calculateSpeed();
 
-		calculateSpeed();
+	generateAngle();
 
-		generateAngle();
+	startStrategy(15);
+    }
 
-		startStrategy(15);
+    private static String readFromSystemIn() throws IOException {
+	String response = "";
+	int nextByte = System.in.read();
+	while (nextByte != -1 && nextByte != '\n') {
+	    response += (char) nextByte;
+	    nextByte = System.in.read();
+	}
+	return response;
+    }
+
+    private static void setDepot() {
+	Location removeLocation = null;
+	for (Location location : nodes) {
+	    if (location.isDepto()) {
+		depot = location;
+		removeLocation = location;
+	    }
+	}
+	nodes.remove(removeLocation);
+    }
+
+    public static double getDistance(Location location1, Location location2) {
+	double lat1 = location1.latitude;
+	double long1 = location1.longitude;
+	double lat2 = location2.latitude;
+	double long2 = location2.longitude;
+	double lat = (lat1 + lat2) / 2 * 0.01745;
+	float latDifference = (float) (111.3 * (lat1 - lat2));
+	float longDifference = (float) (111.3 * Math.cos(lat) * (long1 - long2));
+	return Math.sqrt(latDifference * latDifference + longDifference * longDifference);
+    }
+
+    public static int getDuration(double distance) {
+	int duration = (int) ((distance / speed) * 60);
+	return duration;
+    }
+
+    public static void generateAngle() {
+	double x0 = depot.longitude;
+	double y0 = depot.latitude;
+	for (Location location : nodes) {
+	    double dx = x0 - location.longitude;
+	    double dy = y0 - location.latitude;
+	    if (dx >= 0 && dy >= 0) {
+		location.angle = (Math.toDegrees(Math.atan(dy / dx)));
+	    } else if (dx < 0 && dy >= 0) {
+		location.angle = (Math.toDegrees(Math.atan(dy / dx)) + 180);
+	    } else if (dx < 0 && dy < 0) {
+		location.angle = (Math.toDegrees(Math.atan(dy / dx)) + 180);
+	    } else if (dx >= 0 && dy < 0) {
+		location.angle = (Math.toDegrees(Math.atan(dy / dx)) + 360);
+	    }
+	}
+    }
+
+    private static void calculateSpeed() {
+	int distance = 0;
+	int duration = 0;
+	for (int i = 0; i < nodes.size(); i++) {
+	    distance += getDistance(depot, nodes.get(i));
+	    duration += ((nodes.get(i).timeToDepot + nodes.get(i).timeFromDepot) / 2);
+	}
+	speed = distance * 60 / (duration);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void startStrategy(int slices) {
+
+	ArrayList<Tour> allTours = new ArrayList<>();
+
+	ArrayList<Location> workCopy = (ArrayList<Location>) nodes.clone();
+
+	workCopy = (ArrayList<Location>) nodes.clone();
+	while (!workCopy.isEmpty()) {
+	    allTours.add(findWorkDay(workCopy, slices));
 	}
 
-	private static String readFromSystemIn() throws IOException {
-		String response = "";
-		int nextByte = System.in.read();
-		while (nextByte != -1 && nextByte != '\n') {
-			response += (char) nextByte;
-			nextByte = System.in.read();
-		}
-		return response;
+	int durationOverall = 0;
+	for (Tour tour : allTours) {
+	    durationOverall += tour.getDuration();
 	}
 
-	private static void setDepot() {
-		Location removeLocation = null;
-		for (Location location : nodes) {
-			if (location.isDepto()) {
-				depot = location;
-				removeLocation = location;
-			}
-		}
-		nodes.remove(removeLocation);
+	postSolution(durationOverall);
+    }
+
+    private static void postSolution(int s) {
+	solution = s;
+	System.out.println("SOLUTION " + solution);
+    }
+
+    private static Tour findWorkDay(ArrayList<Location> locations, int slices) {
+	Tour tour = new Tour();
+	while (tour.findingNextStop(locations, slices)) {
+
+	}
+	if (locations.isEmpty()) {
+	    tour.addDepot();
 	}
 
-	public static double getDistance(Location location1, Location location2) {
-		double lat1 = location1.latitude;
-		double long1 = location1.longitude;
-		double lat2 = location2.latitude;
-		double long2 = location2.longitude;
-		double lat = (lat1 + lat2) / 2 * 0.01745;
-		float latDifference = (float) (111.3 * (lat1 - lat2));
-		float longDifference = (float) (111.3 * Math.cos(lat) * (long1 - long2));
-		return Math.sqrt(latDifference * latDifference + longDifference * longDifference);
+	return tour;
+    }
+
+    public static Location findClosestLocation(Location location, ArrayList<Location> locations) {
+	Location returnLocation = locations.get(0);
+	double returnDistance = getDistance(returnLocation, location);
+	for (int i = 1; i < locations.size(); i++) {
+	    double currentDistance = getDistance(location, locations.get(i));
+	    if ((currentDistance < returnDistance)) {
+		returnLocation = locations.get(i);
+		returnDistance = currentDistance;
+	    }
 	}
-	
-	public static int getDuration(double distance) {
-		int duration = (int) ((distance / speed) * 60);
-		return duration;
+	returnLocation.durationToBacker = getDuration(returnDistance);
+	return returnLocation;
+    }
+
+    public static Location findFarthestLocation(Location location, ArrayList<Location> locations) {
+	Location returnLocation = locations.get(0);
+	double returnDistance = getDistance(returnLocation, location);
+	for (int i = 1; i < locations.size(); i++) {
+	    double currentDistance = getDistance(location, locations.get(i));
+	    if ((currentDistance > returnDistance)) {
+		returnLocation = locations.get(i);
+		returnDistance = currentDistance;
+	    }
 	}
-
-	public static void generateAngle() {
-		double x0 = depot.longitude;
-		double y0 = depot.latitude;
-		for (Location location : nodes) {
-			double dx = x0 - location.longitude;
-			double dy = y0 - location.latitude;
-			if (dx >= 0 && dy >= 0) {
-				location.angle = (Math.toDegrees(Math.atan(dy / dx)));
-			} else if (dx < 0 && dy >= 0) {
-				location.angle = (Math.toDegrees(Math.atan(dy / dx)) + 180);
-			} else if (dx < 0 && dy < 0) {
-				location.angle = (Math.toDegrees(Math.atan(dy / dx)) + 180);
-			} else if (dx >= 0 && dy < 0) {
-				location.angle = (Math.toDegrees(Math.atan(dy / dx)) + 360);
-			}
-		}
-	}
-	
-	private static void calculateSpeed() {
-		int distance = 0;
-		int duration = 0;
-		for (int i = 0; i < nodes.size(); i++) {
-			distance += getDistance(depot, nodes.get(i));
-			duration += ((nodes.get(i).timeToDepot + nodes.get(i).timeFromDepot) / 2);
-		}
-		speed = distance*60 / (duration);
-	}
-	
-	@SuppressWarnings("unchecked")
-	private static void startStrategy(int slices) {
-
-		ArrayList<Tour> allTours = new ArrayList<>();
-
-		ArrayList<Location> workCopy = (ArrayList<Location>) nodes.clone();
-
-		workCopy = (ArrayList<Location>) nodes.clone();
-		while (!workCopy.isEmpty()) {
-			allTours.add(findWorkDay(workCopy, slices));
-		}
-
-		int durationOverall = 0;
-		for (Tour tour : allTours) {
-			durationOverall += tour.getDuration();
-		}
-
-		postSolution(durationOverall);
-	}
-
-	private static void postSolution(int s) {
-		solution = s;
-		System.out.println("SOLUTION " + solution);
-	}
-
-	private static Tour findWorkDay(ArrayList<Location> locations, int slices) {
-		Tour tour = new Tour();
-		while (tour.findingNextStop(locations, slices)) {
-
-		}
-		if (locations.isEmpty()) {
-			tour.addDepot();
-		}
-
-		return tour;
-	}
-
-	public static Location findClosestLocation(Location location, ArrayList<Location> locations) {
-		Location returnLocation = locations.get(0);
-		double returnDistance = getDistance(returnLocation, location);
-		for (int i = 1; i < locations.size(); i++) {
-			double currentDistance = getDistance(location, locations.get(i));
-			if ((currentDistance < returnDistance)) {
-				returnLocation = locations.get(i);
-				returnDistance = currentDistance;
-			}
-		}
-		returnLocation.durationToBacker = getDuration(returnDistance);
-		return returnLocation;
-	}
-
-	public static Location findFarthestLocation(Location location, ArrayList<Location> locations) {
-		Location returnLocation = locations.get(0);
-		double returnDistance = getDistance(returnLocation, location);
-		for (int i = 1; i < locations.size(); i++) {
-			double currentDistance = getDistance(location, locations.get(i));
-			if ((currentDistance > returnDistance)) {
-				returnLocation = locations.get(i);
-				returnDistance = currentDistance;
-			}
-		}
-		returnLocation.durationToBacker = getDuration(returnDistance);
-		return returnLocation;
-	}
+	returnLocation.durationToBacker = getDuration(returnDistance);
+	return returnLocation;
+    }
 
 }
